116d2b2127fc412b3b1828f6589785b32352cb51
Bruno Coelho <bruno@charliehr.com>
2020-08-01 15:14:17 +0100

    Add basic controller functionality

--- lib/r00lz.rb
+++ lib/r00lz.rb
@@ -3,11 +3,31 @@ require "r00lz/version"
 module R00lz
   class App
     def call(env)
+      kl, act = cont_and_act(env)
+      text = kl.new(env).send(act)
       [
         200,
         { 'Content-Type' => 'text/html' },
-        ["hello from r00lz!"]
+        [text]
       ]
+    end
+
+    private
+
+    def cont_and_act(env)
+      _, con, act, after = env["PATH_INFO"].split('/')
+      con = con.capitalize + "Controller"
+      # Object.const_get(con) --> get class with the name passed.
+      # So it'll get the controller class.
+      [Object.const_get(con), act]
+    end
+  end
+
+  class Controller
+    attr_reader :env
+
+    def initialize(env)
+      @env = env
     end
   end
 
